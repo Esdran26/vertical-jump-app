3cb78faf4c665aa82d5ac8e856adfe0958a026d2
Esdran26 <vargasandres2627@gmail.com>
2020-04-16 12:34:02 -0500

    Adding Principal Text and Buttons

--- src/components/principal-content/PrincipalContent.js
+++ src/components/principal-content/PrincipalContent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/PrincipalContent.css';
 import Menu from '../menu/Menu';
+import PrincipalText from '../principal-text/PrincipalText';
 
 const PrincipalContent = () => {
     return(
         <div className="principal-content">
             <Menu />
+            <PrincipalText />
         </div>
     );
 }
